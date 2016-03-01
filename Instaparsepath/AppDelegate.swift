68826ce4824b4b9e3c2263fce994405023740661
Michael Ceraso <mceraso@fordham.edu>
2016-02-29 22:24:30 -0500

    Initial setup video

--- Instaparsepath/AppDelegate.swift
+++ Instaparsepath/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        Parse.initializeWithConfiguration(
+            ParseClientConfiguration(block: { (configuration:ParseMutableClientConfiguration) -> Void in
+                configuration.applicationId = "InstaParse"
+                configuration.clientKey = "234mmmfmfoaojojfpwhekfjasd9u2039u90ujn"
+                configuration.server = "https://salty-tundra-61805.herokuapp.com/parse"
+            })
+        )
         return true
     }
 
